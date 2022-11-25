7ba585b63f579a4fa99046c3eabe250d10344690
zodiakara <114649190+zodiakara@users.noreply.github.com>
2022-11-25 16:53:23 +0100

    setting up the app, adding header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,39 @@
+import { Navbar, NavDropdown, Nav, Container } from "react-bootstrap";
+import React from "react";
+
+class Header extends React.Component {
+  render() {
+    return (
+      <header>
+        <Navbar bg="dark" expand="lg">
+          <Container fluid>
+            <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="me-auto">
+                <Nav.Link href="#home">Home</Nav.Link>
+                <Nav.Link href="#movies">Movies</Nav.Link>
+                <Nav.Link href="#link">Series</Nav.Link>
+                <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                  <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                  <NavDropdown.Item href="#action/3.2">
+                    Another action
+                  </NavDropdown.Item>
+                  <NavDropdown.Item href="#action/3.3">
+                    Something
+                  </NavDropdown.Item>
+                  <NavDropdown.Divider />
+                  <NavDropdown.Item href="#action/3.4">
+                    Separated link
+                  </NavDropdown.Item>
+                </NavDropdown>
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+      </header>
+    );
+  }
+}
+
+export default Header;
