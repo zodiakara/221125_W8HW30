50005a724aa281cc2789c7b70a7c60cb14cd0413
zodiakara <114649190+zodiakara@users.noreply.github.com>
2022-11-26 20:16:55 +0100

    gallery component update

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import "./App.css";
 import Header from "./components/Header";
 import GalleryItem from "./components/GalleryItem";
 import FooterItem from "./components/FooterItem";
-import ToggleButton from "./components/ToggleButton";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "../src/assets/styles.css";
 
@@ -10,10 +9,9 @@ function App() {
   return (
     <div>
       <Header />
-      <GalleryItem />
-      <GalleryItem />
-      <GalleryItem />
-      <ToggleButton />
+      <GalleryItem description="Movie Series" />
+      <GalleryItem description="Christmas Specials" />
+      <GalleryItem description="New Releases" />
       <FooterItem />
     </div>
   );
--- src/components/GalleryItem.jsx
+++ src/components/GalleryItem.jsx
@@ -1,46 +1,126 @@
-import Carousel from "react-bootstrap/Carousel";
-
-function GalleryItem() {
+function GalleryItem(props) {
   return (
-    <Carousel fade>
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src="holder.js/800x400?text=First slide&bg=373940"
-          alt="First slide"
-        />
-        <Carousel.Caption>
-          <h3>First slide label</h3>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src="holder.js/800x400?text=Second slide&bg=282c34"
-          alt="Second slide"
-        />
-
-        <Carousel.Caption>
-          <h3>Second slide label</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src="holder.js/800x400?text=Third slide&bg=20232a"
-          alt="Third slide"
-        />
-
-        <Carousel.Caption>
-          <h3>Third slide label</h3>
-          <p>
-            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-          </p>
-        </Carousel.Caption>
-      </Carousel.Item>
-    </Carousel>
+    <div className="movie-gallery m-2">
+      <h5 className="text-light mt-2 mb-2">{props.description}</h5>
+      <div id="new-releases" className="carousel slide" data-bs-ride="carousel">
+        <div className="carousel-inner">
+          <div className="carousel-item active">
+            <div className="movie-row">
+              <div className="row">
+                <div className="col-md-2">
+                  <img
+                    className="movie-cover"
+                    src="./assets/media/media12.jpg"
+                    alt="movie-sample"
+                  />
+                </div>
+                <div className="col-md-2">
+                  <img
+                    className="movie-cover"
+                    src="./assets/media/media13.jpg"
+                    alt="movie-sample"
+                  />
+                </div>
+                <div className="col-md-2">
+                  <img
+                    className="movie-cover"
+                    src="./assets/media/media14.jpg"
+                    alt="movie-sample"
+                  />
+                </div>
+                <div className="col-md-2">
+                  <img
+                    className="movie-cover"
+                    src="./assets/media/media15.jpg"
+                    alt="movie-sample"
+                  />
+                </div>
+                <div className="col-md-2">
+                  <img
+                    className="movie-cover"
+                    src="./assets/media/media16.jpg"
+                    alt="movie-sample"
+                  />
+                </div>
+                <div className="col-md-2">
+                  <img
+                    className="movie-cover"
+                    src="./assets/media/media17.jpg"
+                    alt="movie-sample"
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="carousel-item">
+            <div className="movie-row">
+              <div className="row">
+                <div className="col-md-2">
+                  <img
+                    className="movie-cover"
+                    src="./assets/media/media12.jpg"
+                    alt="movie-sample"
+                  />
+                </div>
+                <div className="col-md-2">
+                  <img
+                    className="movie-cover"
+                    src="./assets/media/media13.jpg"
+                    alt="movie-sample"
+                  />
+                </div>
+                <div className="col-md-2">
+                  <img
+                    className="movie-cover"
+                    src="./assets/media/media14.jpg"
+                    alt="movie-sample"
+                  />
+                </div>
+                <div className="col-md-2">
+                  <img
+                    className="movie-cover"
+                    src="./assets/media/media15.jpg"
+                    alt="movie-sample"
+                  />
+                </div>
+                <div className="col-md-2">
+                  <img
+                    className="movie-cover"
+                    src="./assets/media/media16.jpg"
+                    alt="movie-sample"
+                  />
+                </div>
+                <div className="col-md-2">
+                  <img
+                    className="movie-cover"
+                    src="./assets/media/media17.jpg"
+                    alt="movie-sample"
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <button
+          className="carousel-control-prev"
+          type="button"
+          data-bs-target="#new-releases"
+          data-bs-slide="prev"
+        >
+          <span className="carousel-control-prev-icon" aria-hidden="true" />
+          <span className="visually-hidden">Previous</span>
+        </button>
+        <button
+          className="carousel-control-next"
+          type="button"
+          data-bs-target="#new-releases"
+          data-bs-slide="next"
+        >
+          <span className="carousel-control-next-icon" aria-hidden="true" />
+          <span className="visually-hidden">Next</span>
+        </button>
+      </div>
+    </div>
   );
 }
 
